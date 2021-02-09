72283f60d43b680aff0c303147dd6040fe48a3c0
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-02-10 02:22:22 +0530

    Make -hr positional argument

--- backlog.py
+++ backlog.py
@@ -5,14 +5,12 @@ import argparse
 parser = argparse.ArgumentParser(prog="backlog", description="Change time of last commit in git.",
                                  epilog="Visit github.com/2KAbhishek/backlog for more.")
 
-# Positional arguments
-parser.add_argument("hours", metavar="hours", type=int,
+# Optional arguments
+parser.add_argument("-hr", "--hours", metavar="hours", type=int, action="store", default=0,
                     help="Number of hours to go back")
 
-# Optional arguments
 parser.add_argument("-d", "--days", metavar="days", type=int,
                     action="store", default=0, help="Number of days to go back")
-
 
 parser.add_argument("-m", "--minutes", metavar="minutes",
                     type=int, action="store", default=0, help="Number of minutes to go back")
